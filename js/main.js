7b668ffbfc6bf3ebca0ed2c9d35ea36d3e77d2cf
PlastiCode <juanp2000@gmail.com>
2018-07-12 17:53:21 +0200

    .

---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+$("#banner").css({"height":$(window).height()+"px"});// pone el fondo de la misma medida del alto de la ventana
+$("#body").css({"height":$(window).height()+"px"});
+})
