e41a4132c799c2fadde152dfd68208486831a440
Juan Pablo Arias Barboza <juanp2000@gmail.com>
2018-09-03 11:53:27 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,4 +1,63 @@
-$(document).ready(function(){
-$("#banner").css({"height":$(window).height()+"px"});// pone el fondo de la misma medida del alto de la ventana
-$("#body").css({"height":$(window).height()+"px"});
+$(document).ready(function() {
+  /////////////////////////////////////
+  // Menu Scroll function /////////////
+  /////////////////////////////////////
+  $(window).scroll(function() {
+    scroll = $(window).scrollTop();
+    if (scroll > 200) {
+      $("#logo").css({
+        "height": "50px",
+        "margin-top": "-5px"
+      });
+      $("header").css({
+        "background-color": "var(--header)"
+      });
+      $(".via").css({
+        "visibility": "visible"
+      });
+      $("#down").css({
+        "display": "none"
+      });
+
+    } else {
+      $("#logo").css({
+        "height": "30vh",
+        "margin-top": "30vh"
+      });
+      $("header").css({
+        "background-color": "transparent"
+      });
+      $(".via").css({
+        "visibility": "hidden"
+      });
+      $("#down").css({
+        "display": "flex"
+      });
+    }
+  });
+  /////////////////////////////////////
+  // go top function //////////////////
+  /////////////////////////////////////
+  $('.ir-arriba').click(function() {
+    $('body, html').animate({
+      scrollTop: '0px'
+    }, 300);
+  });
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $('.ir-arriba').slideDown(300);
+    } else {
+      $('.ir-arriba').slideUp(300);
+    }
+  });
+  /////////////////////////////////////
+  // Smooth Scroll function ///////////
+  /////////////////////////////////////
+  $('a').smoothScroll({
+    offset: -70,
+    speed: 1200
+  });
+
+  /////////////////////////////////////
+
 })
