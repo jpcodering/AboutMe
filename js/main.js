4378838d23cf329351429f38a221c4490a336e15
Juan Pablo Arias Barboza <juanp2000@gmail.com>
2018-10-26 20:12:04 +0200

    AboutMe
    update work
    
    AboutMe
    
    update work
    
    AboutMe
    
    update work
    
    AboutMe
    
    update work
    
    AboutMe
    
    update work
    
    AboutMe
    
    update work
    
    AboutMe
    
    update work
    
    AboutMe
    
    update work
    
    AboutMe
    
    update work
    
    AboutMe
    
    update work
    
    AboutMe
    
    update work

--- js/main.js
+++ js/main.js
@@ -1,47 +1,5 @@
 $(document).ready(function() {
   /////////////////////////////////////
-  // Menu Scroll function /////////////
-  /////////////////////////////////////
-  $(window).scroll(function() {
-    scroll = $(window).scrollTop();
-    if (scroll > 200) {
-      $("#logo").css({
-        "height": "50px",
-        "margin-top": "-5px"
-      });
-      $("header").css({
-        "background-color": "var(--header)"
-      });
-      $(".via").css({
-        "visibility": "visible"
-      });
-      $("header>nav>ul").css({
-        "display": "block"
-      });
-      $("#down").css({
-        "display": "none"
-      });
-
-    } else {
-      $("#logo").css({
-        "height": "30vh",
-        "margin-top": "30vh"
-      });
-      $("header").css({
-        "background-color": "transparent"
-      });
-      $("header>nav>ul").css({
-        "display": "none"
-      });
-      $(".via").css({
-        "visibility": "hidden"
-      });
-      $("#down").css({
-        "display": "flex"
-      });
-    }
-  });
-  /////////////////////////////////////
   // go top function //////////////////
   /////////////////////////////////////
   $('.ir-arriba').click(function() {
@@ -49,6 +7,7 @@ $(document).ready(function() {
       scrollTop: '0px'
     }, 300);
   });
+
   $(window).scroll(function() {
     if ($(this).scrollTop() > 0) {
       $('.ir-arriba').slideDown(300);
@@ -56,11 +15,12 @@ $(document).ready(function() {
       $('.ir-arriba').slideUp(300);
     }
   });
+
   /////////////////////////////////////
   // Smooth Scroll function ///////////
   /////////////////////////////////////
   $('a').smoothScroll({
-    offset: -70,
+    // offset: -70,
     speed: 1200
   });
 
